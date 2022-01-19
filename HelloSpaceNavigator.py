cd756215e510dfd6699a4e03fd20cda8bf020675
wildan.gifari <wildan@ai.iit.tsukuba.ac.jp>
2022-01-19 02:03:08 +0000

    change mouse to universal receiver

--- HelloSpaceNavigator.py
+++ HelloSpaceNavigator.py
@@ -7,7 +7,7 @@ from time import gmtime, strftime
 import time
 
 # Look for SpaceNavigator
-dev = usb.core.find(idVendor=0x46d, idProduct=0xc62b)
+dev = usb.core.find(idVendor=0x256f, idProduct=0xc652)
 if dev is None:
     raise ValueError('SpaceNavigator not found');
 else:
